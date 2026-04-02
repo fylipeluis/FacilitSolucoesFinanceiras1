9b0579403de21ac6e2a160264781621f0702fd11
Luis Fylipe <luisfylipe1802@gmail.com>
2026-04-02 14:41:15 -0300

    feat(pagina-adm): Criando e estilizando titulo do header na pagina de administrador.

--- Facilit/src/Administrador/HeaderPaginaAdministrador/Header.tsx
+++ Facilit/src/Administrador/HeaderPaginaAdministrador/Header.tsx
@@ -1,8 +1,10 @@
 import "./Header.css"
+import { TituloHeaderPaginaAdm } from "./HeaderComponents/TituloHeaderPaginaAdm"
 
 export default function HeaderPaginaAdministrador(){
     return (
         <div className="header">
+            <TituloHeaderPaginaAdm />
         </div>
     )
 }
---
+++ Facilit/src/Administrador/HeaderPaginaAdministrador/HeaderComponents/TituloHeaderPaginaAdm.tsx
@@ -0,0 +1,9 @@
+import "./TituloHeaderPaginaAdm.css"
+
+export function TituloHeaderPaginaAdm(){
+    return(
+        <div className="title-header">
+            <h1>Gerenciamento de Clientes</h1>
+        </div>
+    )
+}
