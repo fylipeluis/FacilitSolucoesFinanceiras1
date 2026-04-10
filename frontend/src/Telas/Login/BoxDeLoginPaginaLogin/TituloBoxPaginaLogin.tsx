37f002e1d36ba3f365bb299dd2085de3ea3dc600
Luis Fylipe <luisfylipe1802@gmail.com>
2026-04-10 17:34:02 -0300

    feat(pagina-login): Criando titulo do box de dado de entrada da pagina de login.

---
+++ frontend/src/Telas/Login/BoxDeLoginPaginaLogin/TituloBoxPaginaLogin.tsx
@@ -0,0 +1,10 @@
+import "./TituloBoxPaginaLogin.css";
+
+export function TituloBoxPaginaLogin() {
+  return (
+    <div className="title">
+      <h2>Bem-vindo</h2>
+      <p> Digite seu CPF</p>
+    </div>
+  );
+}
