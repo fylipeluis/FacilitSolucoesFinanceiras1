08b7b17a3992a92c356da90ea1ae30f7b4a8956f
Luis Fylipe <luisfylipe1802@gmail.com>
2026-04-21 13:01:30 -0300

    feat(pagina-login): Criando header da pagina de login para colocar logo.

--- frontend/src/Telas/Login/Login.tsx
+++ frontend/src/Telas/Login/Login.tsx
@@ -1,9 +1,11 @@
 import BoxDeLoginPaginaLogin from "./BoxDeLoginPaginaLogin/BoxDeLoginPaginaLogin";
+import { HeaderPaginaDeLogin } from "./HeaderDePaginaLogin/HeaderPaginaDeLogin";
 import "./Login.css";
 
 export function PaginaDeLogin() {
   return (
     <div className="login">
+      <HeaderPaginaDeLogin />
       <BoxDeLoginPaginaLogin/>
     </div>
   );
