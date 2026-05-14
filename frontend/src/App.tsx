9de958e2ba0462706515e693253f36c6a3294ba4
Luis Fylipe <luisfylipe1802@gmail.com>
2026-05-14 08:51:15 -0300

    feat(pagina-login): Refatoração da caixa de inputs da pagina de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,32 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import PaginaAdministrador from "./Telas/Administrador/Adm"
-import { PaginaDeLogin } from "./Telas/Login/login";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import PaginaAdministrador from "./Telas/Administrador/Adm";
+import { PaginaDeLogin } from "./Telas/Login/PaginaDeLogin";
+
+// EXEMPLO TEMPORÁRIO DA PÁGINA CLIENTE
+function PaginaCliente() {
+  return <h1>Página do Cliente</h1>;
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Navigate to="/login" />} />
-    <Route path="/login" element={<PaginaAdministrador />}/>
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<PaginaDeLogin />} />
+
+        <Route
+          path="/administrador"
+          element={<PaginaAdministrador />}
+        />
+
+        <Route
+          path="/cliente"
+          element={<PaginaCliente />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
